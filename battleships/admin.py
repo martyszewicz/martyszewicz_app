b4c8de5fe243a3cf4272588a9c8ccfdc3f413411
martyszewicz <m.artyszewicz@gmail.com>
2023-11-30 09:06:23 +0100

    Upgrade animations in coffee machine app

--- battleships/admin.py
+++ battleships/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from .models import Game
+from .models import BattleshipsRoom, TrackPlayers
 
 
-class GameAdmin(admin.ModelAdmin):
-    list_display = ["room_code", "game_creator", "game_opponent", "is_private", "password", "is_over", "is_over"]
+class BattleshipsRoomAdmin(admin.ModelAdmin):
+    list_display = ["room_name"]
 
-admin.site.register(Game, GameAdmin)
+
+class TrackPlayersAdmin(admin.ModelAdmin):
+    list_display = ["username"]
+
+
+
+admin.site.register(BattleshipsRoom, BattleshipsRoomAdmin)
+admin.site.register(TrackPlayers, TrackPlayersAdmin)
